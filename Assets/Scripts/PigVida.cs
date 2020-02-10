da4db2c460f0f82566d53cc7d52eee32ce9d4ece
Albert <Porta>
2020-02-10 18:02:11 +0100

    ...

---
+++ Assets/Scripts/PigVida.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigVida : MonoBehaviour
+{
+    int vida;
+    float velocidad;
+
+    [Range(0, 20)] public int default_vida;
+    [Range(0f, 5f)] public float default_velocidad;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        InicializarCerdo();
+    }
+
+    // Update is called once per frame
+    public void InicializarCerdo()
+    {
+        if (vida != default_vida) vida = default_vida;
+        if (velocidad != default_velocidad) velocidad = default_velocidad;
+    }
+
+    /// <summary>
+    /// Establece la vida
+    /// </summary>
+    /// <param name="val">cantidad, por defecto es 5</param>
+    public void SetVida(int val = 5) {
+        vida = Mathf.Clamp(val, 0, 20);
+    }
+
+    public void SetVelocidad(float val = 3f) {
+        velocidad = Mathf.Clamp(val, 0f, 5f);
+    }
+
+    public int GetVida()
+    {
+        return vida;
+    }
+    public float GetVelocidad()
+    {
+        return velocidad;
+    }
+
+
+    public bool isAlive()
+    {
+        return vida > 0;
+    }
+
+
+
+}
